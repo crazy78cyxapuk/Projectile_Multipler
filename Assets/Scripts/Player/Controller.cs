02a2e16c30809068e9df8897aeb66bcdf40fdf3c
OlegCyxapuk <olezhki.net@gmail.com>
2022-04-01 18:13:33 +0300

    added movement to side

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -8,9 +8,14 @@ namespace Player
 {
     public class Controller : MonoBehaviour
     {
-        [SerializeField] private List<Movement> _allMovements = new List<Movement>();
+        [SerializeField] private Movement _movement;
         [SerializeField] private ArrowSpawn _arrowSpawn;
 
+        private Vector2 _startPos;
+        private Vector2 _direction;
+        private float _minDistanceTouch = 3f;
+        private float _deceleration = 350f;
+        
         private void Awake()
         {
             //_arrowPosition.AddArrows(1);
@@ -23,25 +28,71 @@ namespace Player
 
         private void CheckInput()
         {
-            if (Input.GetMouseButton(0))
-            {
-                //StartAllMovements();
-                _arrowSpawn.AddArrows(1);
-            }
+            // if (Input.GetMouseButtonDown(0))
+            // {
+            //     _startPos = Input.mousePosition;
+            // }
+            //
+            // if (Input.GetMouseButton(0))
+            // {
+            //     CheckMousePosition();
+            // }
+            
+            CheckInputMobile();
             
             if (Input.GetMouseButton(1))
             {
                 //StartAllMovements();
-                _arrowSpawn.RemoveArrow(1);
+                //_arrowSpawn.RemoveArrow(1);
+                _arrowSpawn.AddArrows(1);
+            }
+        }
+
+        private void CheckInputMobile()
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        _startPos = Input.mousePosition;
+                        break;
+                    
+                    case TouchPhase.Moved:
+                        CheckMousePosition();
+                        break;
+                    
+                    case TouchPhase.Stationary:
+                        _startPos = Input.mousePosition;
+                        break;
+                }
+            }
+        }
+
+        private void CheckMousePosition()
+        {
+            Vector2 mousePos = Input.mousePosition;
+            _direction = (mousePos - _startPos) / _deceleration;
+
+            float distanceMouse = Vector2.Distance(_startPos, mousePos);
+            if (distanceMouse > _minDistanceTouch)
+            {
+                if (_direction.x > 0)
+                {
+                    _movement.MoveToSide(TargetSide.Right);
+                }
+                else
+                {
+                    _movement.MoveToSide(TargetSide.Left);
+                }
             }
         }
 
         private void StartAllMovements()
         {
-            for (int i = 0; i < _allMovements.Count; i++)
-            {
-                _allMovements[i].StartMove();
-            }
+            
         }
     }
 }
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 namespace Player
 {
+    public enum TargetSide
+    {
+        Left, Right
+    }
+    
     public class Movement : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private float _speedToSide;
         [SerializeField] private Vector3 _direction;
+        [SerializeField] private float _maxDistanceToSide = 4.5f;
 
         private Movement _movement;
 
@@ -30,12 +37,42 @@ namespace Player
 
         public void StopMove()
         {
-
+            _movement.enabled = false;
         }
 
         private void MoveForward()
         {
             transform.position += _direction * _speed * Time.deltaTime;
+        }
+
+        public void MoveToSide(TargetSide target)
+        {
+            switch (target)
+            {
+                case TargetSide.Left:
+
+                    float targetLeft = _speedToSide * Time.deltaTime;
+                    float offsetLeft = transform.localPosition.z - targetLeft;
+
+                    if (offsetLeft > -_maxDistanceToSide)
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y, offsetLeft);
+                    }
+                    
+                    break;
+                
+                case TargetSide.Right:
+                    
+                    float targetRight = _speedToSide * Time.deltaTime;
+                    float offsetRight = transform.localPosition.z + targetRight;
+
+                    if (offsetRight < _maxDistanceToSide)
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y, offsetRight);
+                    }
+                    
+                    break;
+            }
         }
     }
 }
