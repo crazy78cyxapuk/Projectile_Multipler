e3b042c6da5879131be8bb009824dfc65d84dffa
OlegCyxapuk <olezhki.net@gmail.com>
2022-04-19 14:43:36 +0300

    fix

--- Assets/Scripts/Dev/Enemy/Rocket/RocketExplosion.cs
+++ Assets/Scripts/Dev/Enemy/Rocket/RocketExplosion.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Extension;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Rocket
 {
@@ -9,7 +11,16 @@ namespace Rocket
     {
         [SerializeField] private PoolObject _poolObject;
         [SerializeField] private CameraData _cameraData;
-        
+        [SerializeField] private TurnController _turnController;
+
+        private UnityAction _explosion;
+
+        private void Awake()
+        {
+            _explosion += () => Explosion(false);
+            _turnController.AddAction(_explosion);
+        }
+
         public void Explosion(bool isTurnShake)
         {
             gameObject.SetActive(false);
