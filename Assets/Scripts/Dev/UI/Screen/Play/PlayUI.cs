beff4bb7b2d79f0cf8b843eac3bde036dc45b08a
OlegCyxapuk <olezhki.net@gmail.com>
2022-05-02 11:36:48 +0300

    added help finger

--- Assets/Scripts/Dev/UI/Screen/Play/PlayUI.cs
+++ Assets/Scripts/Dev/UI/Screen/Play/PlayUI.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class PlayUI : MonoBehaviour
 {
+    [SerializeField] private TurnController _turnController;
+    [SerializeField] private GameObject _helpPanel;
+
+    private UnityAction _turnHelpPanel;
+
+    private void Awake()
+    {
+        _turnHelpPanel += TurnHelpPanel;
+        _turnController.AddAction(_turnHelpPanel);
+    }
+
     public void RepeatLevel()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void TurnHelpPanel()
+    {
+        _helpPanel.SetActive(true);
     }
 }
